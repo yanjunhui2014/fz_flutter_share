3fd46e20bb0bf493dfbe693b8dfa5a36659f41da
milo <303767416@qq.com>
2021-08-12 15:57:29 +0800

    fix 1、去除影响我打包aar的依赖；

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:path_provider/path_provider.dart';
 
 class Utils {
   Utils._();
@@ -16,24 +15,22 @@ class Utils {
 
   String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
 
-  Future<String> getStoragePath() async {
-    Directory directory;
-    if (Platform.isAndroid) {
-      directory = await getExternalStorageDirectory();
-    } else if (Platform.isIOS) {
-      directory = await getTemporaryDirectory();
-    }
+  // Future<String> getStoragePath() async {
+  //   Directory directory;
+  //   if (Platform.isAndroid) {
+  //     directory = await getExternalStorageDirectory();
+  //   } else if (Platform.isIOS) {
+  //     directory = await getTemporaryDirectory();
+  //   }
+  //
+  //   if (directory != null) {
+  //     return directory.path;
+  //   }
+  // }
 
-    if (directory != null) {
-//      String value = directory.path + "/flutter/thinking";
-//      Directory stoDirectory = await Directory(value).create(recursive: true);
-      return directory.path;
-    }
-  }
-
-  Future<String> cachePath() async {
-    return await getStoragePath().then((value) => value + "/cache");
-  }
+  // Future<String> cachePath() async {
+  //   return await getStoragePath().then((value) => value + "/cache");
+  // }
 
   static bool isEmpty(Object object){
     if(object == null){
